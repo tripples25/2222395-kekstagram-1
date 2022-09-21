42a57054e9ed5a2d149a95467198fb0acd54a2a7
tripples25 <92451706+tripples25@users.noreply.github.com>
2022-09-21 17:40:59 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function getRandomInt(min, max) {
+function GetRandomInt(min, max) {
   return Math.floor( Math.random() * (max - min + 1) + min);
 }
 
-function checkLength(commentary, maxLength)
+function CheckLength(commentary, maxLength)
 {
   return commentary.length <= maxLength;
 }
