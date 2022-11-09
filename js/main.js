bb3b0a122eb88dee1218c82d08fb3125905bcba8
tripples25 <92451706+tripples25@users.noreply.github.com>
2022-11-09 15:08:14 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const randomComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const names = ['Артём', 'Андрей', 'Наруто'];
+const RANDOM_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const NAMES = ['Артём', 'Андрей', 'Наруто'];
 let lastCommentIndex = 1;
 
 function getRandomInt(min, max)
@@ -15,13 +15,15 @@ function checkLength(commentary, maxLength)
 function generateComments(count)
 {
   const comments = [];
+  const minPictureIndex = 1;
+  const maxPuctureIndex = 6;
   for (let i = 0; i < count; i++)
   {
     const comment = {
       id: lastCommentIndex++,
-      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-      message: randomComments[getRandomInt(0, randomComments.length - 1)],
-      name: names[getRandomInt(0, names.length - 1)]
+      avatar: `img/avatar-${getRandomInt(minPictureIndex, maxPuctureIndex)}.svg`,
+      message: randomComments[getRandomInt(0, RANDOM_COMMENTS - 1)],
+      name: names[getRandomInt(0, NAMES.length - 1)]
     };
     comments.push(comment);
   }
@@ -31,20 +33,22 @@ function generateComments(count)
 function generatePhotos(count)
 {
   const photos = [];
+  const minLikesCount = 15;
+  const maxLikesCount = 200;
+  const commentsCount = 10;
   for (let i = 0; i < count; i++)
   {
     const photo = {
       id: i + 1,
       url: `photos/${i + 1}.jpg`,
       description: 'Мудрое дерево.',
-      likes: getRandomInt(15, 200),
-      comments: generateComments(10)
+      likes: getRandomInt(minLikesCount, maxLikesCount),
+      comments: generateComments(commentsCount)
     };
     photos.push(photo);
   }
   return photos;
 }
 
-getRandomInt(0, 10);
 checkLength('abx', 10);
 generatePhotos(25);
