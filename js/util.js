373e47a71c84b22d4d979ccf394a83e3fdd862b6
tripples25 <92451706+tripples25@users.noreply.github.com>
2022-12-22 15:00:28 +0500

    Добавлен валидатор формы загрузки изображения

--- js/util.js
+++ js/util.js
@@ -1,5 +1,30 @@
-export const getRandomInt = (min, max) => Math.floor( Math.random() * (max - min + 1) + min);
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-export const checkLength = (commentary, maxLength) => commentary.length <= maxLength;
+const checkLength = (commentary, maxLength) => {
+  return commentary.length <= maxLength;
+};
 
-export const isEscapeKeyPressed = (evt) => evt.key === 'Escape';
+const arrayContainsElement = (array, element) => {
+  return array.indexOf(element) >= 0;
+};
+
+const isEscapeKeyPressed = (evt) => {
+  return evt.key === 'Escape';
+};
+
+const anyElementIsDuplicated = (array) => {
+  const temp = array.slice();
+  const length = temp.length;
+  for (let i = 0; i < length; i++) {
+    const element = temp[0];
+    temp.splice(0, 1);
+    if (arrayContainsElement(temp, element)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export {getRandomInt, checkLength, isEscapeKeyPressed, anyElementIsDuplicated};
